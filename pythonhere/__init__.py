f70721f6423c760db1f6a7625dfa6d05dea7ac07
b3b <ash.b3b@gmail.com>
2020-12-19 19:06:39 +0300

    expose package version

--- pythonhere/__init__.py
+++ pythonhere/__init__.py
@@ -2,6 +2,7 @@
 from herethere.magic import load_ipython_extension
 
 from .magic_here import shortcuts  # noqa
+from .version_here import __version__  # noqa
 
 
 __all__ = ("load_ipython_extension",)
