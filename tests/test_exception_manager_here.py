7abdc0bf76077f76bd5209a98168dbe70f071da9
b3b <ash.b3b@gmail.com>
2026-05-18 22:08:31 +0200

    Make exception popup test deterministic

--- tests/test_exception_manager_here.py
+++ tests/test_exception_manager_here.py
@@ -16,5 +16,5 @@ async def test_popup_shown_on_app_exception(app_instance, there):
     assert app_instance.get_running_app()
     await there.runcode("from kivy.clock import Clock")
     await there.runcode("Clock.schedule_once(lambda dt: 1 / 0, -1)")
-    await there.runcode("Clock.idle()")
+    await there.runcode("Clock.tick_draw()")
     assert isinstance(app_instance._app_window.children[0], UnhandledExceptionPopupHere)
